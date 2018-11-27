5da62609932c85563b122448695e8874ff3f0347
Francis Maile <francismaile@gmail.com>
2018-11-26 20:33:44 -0800

    did it without using Array.reduce...

---
+++ Consecutive_strings/longestConsec.js
@@ -0,0 +1,30 @@
+function longestConsec(strarr, k) {
+	if( strarr.length===0 || k > strarr.length || k <= 0) return '';
+	let longest = '', attempt = '';
+	strarr.forEach( (c,i,a) => {
+		attempt = a.slice(i,i+k).join('');
+		longest = longest.length >= attempt.length || i+k > strarr.length ? longest : attempt;
+	});
+	return longest;
+}
+
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2));	
+console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh");
+console.log(longestConsec([], 3), "");
+console.log(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
+
+/* Output */
+abigailtheta
+oocccffuucccjjjkkkjyyyeehh oocccffuucccjjjkkkjyyyeehh
+ 
+wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck
+wlwsasphmxxowiaxujylentrklctozmymu wlwsasphmxxowiaxujylentrklctozmymu
+ 
+ixoyx3452zzzzzzzzzzzz ixoyx3452zzzzzzzzzzzz
+ 
+ 
