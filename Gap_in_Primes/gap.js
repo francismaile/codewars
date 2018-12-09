d4ba9459d1559fbef11608452d59672d13fe6efa
Francis Maile <francismaile@gmail.com>
2018-12-09 08:13:13 -0800

    an over-engineered solution, but it works

--- Gap_in_Primes/gap.js
+++ Gap_in_Primes/gap.js
@@ -1,27 +1,43 @@
-function gap(g, m, n) {
-	let primes = Array.from({ length: (n - 2) }, (_, i) => 2 + i);
-	// sieve of eratosthenes
-	let p = 2;
-	let index = 0;
-	while( index <= n && index >= 0 ) {
-		for( let i = index; i <= n; i += p) {
-			if( primes[i] !== p) primes[i] = 0;
-		}
-		index = primes.findIndex( num => num > p );
-		p = primes[index];
+function isPrime(num) {
+	if(num <= 3) {
+		return true;
+	} else if( ! (num % 2) || ! (num % 3) ) {
+		return false;
 	}
-
-	primes =  primes.filter( num => num > 0 );
-	primes = primes.slice(primes.findIndex(e => e >= m) )
-	index = primes.findIndex( (e, i, a) => a[i+1] - e === g );
-
-	return index < 0 ? null : [primes[index], primes[index+1]];
+	let i = 5;
+	while(i * i <= num) {
+		if( num % i === 0 || num % (i+2) === 0  ) {
+			return false;
+		} else {
+			i = i + 6;
+		}
+	}
+	return true;
 }
 
+function gap(g, m, n) {
+	let first = 0;
+	for( let i = m; i <= n; i++ ) {
+		if( isPrime(i) ) {
+			if(first) {
+				if( i - first === g ) return [first, i];
+				else first = i;
+			} else {
+				first = i;
+			}
+		}
+	}
+	return null;
+}
+
+
+console.log(gap(2,100,110), [101, 103]);
+console.log(gap(4,100,110), [103, 107]);
+console.log(gap(6,100,110), null);
+console.log(gap(8,300,400), [359, 367]);
 console.log(gap(10,300,400), [337, 347]);
 
 /*
-
 console.log(gap(2,100,110), [101, 103]);
 console.log(gap(4,100,110), [103, 107]);
 console.log(gap(6,100,110), null);
