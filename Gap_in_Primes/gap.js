c59e248700e9a4f6c7a48912e3cec113d9af85db
Francis Maile <francismaile@gmail.com>
2018-12-09 08:37:16 -0800

    some code cleanup. removed unecessay code

--- Gap_in_Primes/gap.js
+++ Gap_in_Primes/gap.js
@@ -19,12 +19,8 @@ function gap(g, m, n) {
 	let first = 0;
 	for( let i = m; i <= n; i++ ) {
 		if( isPrime(i) ) {
-			if(first) {
-				if( i - first === g ) return [first, i];
-				else first = i;
-			} else {
-				first = i;
-			}
+			if( i - first === g ) return [first, i];
+			else first = i;
 		}
 	}
 	return null;
@@ -37,11 +33,4 @@ console.log(gap(6,100,110), null);
 console.log(gap(8,300,400), [359, 367]);
 console.log(gap(10,300,400), [337, 347]);
 
-/*
-console.log(gap(2,100,110), [101, 103]);
-console.log(gap(4,100,110), [103, 107]);
-console.log(gap(6,100,110), null);
-console.log(gap(8,300,400), [359, 367]);
-console.log(gap(10,300,400), [337, 347]);
-*/
 
