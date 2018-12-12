763a0ba2af6dae400ce035ef829341fb6a59624d
Francis Maile <francismaile@gmail.com>
2018-12-12 15:34:57 -0800

    works for all tests

--- Double_Cola/whoIsNext.js
+++ Double_Cola/whoIsNext.js
@@ -1,16 +1,45 @@
 function whoIsNext(names, r) {
-  let colaQueue = Array(names.length).fill(1).map( (e, i) => e + i);
-	let moving = 0;
-	for( let i = 1; i < r; i++ ){
-		moving = colaQueue.shift();
-		colaQueue.push(moving, moving);
+	let x = r, m = 1, q = 0;
+	if(r <= names.length ) return names[r-1];
+	while( x >= q ) {
+		q = names.length * m;
+		if( x-q < 0 ) break;
+		x = x - q;
+		m = m * 2;
+	} 
+	if( x % m !== 0 ) {
+		return names[Math.floor(x/m)];
+	} else {
+		return names[(x/m)-1];
 	}
-	return names[colaQueue[0] - 1];
 }
 
-console.log(whoIsNext( ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52 ) );
+
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 8786));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 61));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 84508451));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 544555));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 160551));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 42239510));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 71));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 18684217));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 9297905));
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 1) ) // == "Sheldon"
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52) ) // == "Penny"
+console.log(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951) ) // == "Leonard"
 
 /* Output
 
-
+Rajesh
+Rajesh
+Sheldon
+Rajesh
+Howard
+Sheldon
+Howard
+Rajesh
+Rajesh
+Sheldon
+Penny
+Leonard
 */
