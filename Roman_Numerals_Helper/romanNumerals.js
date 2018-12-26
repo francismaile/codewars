8b49e55b86cdd567aba4cf86afd9789c845a603a
Francis Maile <francismaile@gmail.com>
2018-12-25 16:03:03 -0800

    finished with this challenge

--- Roman_Numerals_Helper/romanNumerals.js
+++ Roman_Numerals_Helper/romanNumerals.js
@@ -1,56 +1,77 @@
 let RomanNumerals = (function() {
 
 	const romanDigits = {
-		'M': 1000,
-		'D': 500,
-		'C': 100,
-		'L': 50,
-		'X': 10,
-		'V': 5,
-		'I': 1
+		'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
+		'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
+		'X': 10, 'IX': 9, 'V': 5, 'IV':4, 'I': 1
 	}
 
-	let convertRoman = (num) => {
-		return Object.entries(romanDigits).find( (elem) => {
-			[key, value] = elem;
-			// console.log( key );
-			return num === value ? key : null;
-		});
-		// forEach( ([key, value] ) => console.log( {key}, {value} ) );
-	}
 	return {
-		keys: function() {
-			return Object.keys(romanDigits);
-		},
-		entries: function() {
-			return Object.entries(romanDigits).forEach( ([key, value] ) => console.log( {key}, {value} ) );
-		},
-		toRoman: function(integer) {
-			
+		toRoman: function(number) {
+			const romans = Object.entries(romanDigits);
+			let i = 0;
+			let quotient = 0;
+			let remainder = 0;
+			let romanNumeral = [];
+			while( number > 0 ) {
+				[roman, decimal] = romans[i];
+				if( decimal <= number ) {
+					quotient = Math.floor(number/decimal);
+					remainder = number % decimal;
+					number -= quotient * decimal;
+					romanNumeral = [...romanNumeral,...Array(quotient).fill(roman)];
+				}
+				i++;
+			}
+			return romanNumeral.join('');
 		},
 		fromRoman: function(roman) {
-			let digital = 0;
+			let decimal = 0;
 			for( let i = 0; i < roman.length; i++ ) {
 				if( i+1 < roman.length ) {
 					if( romanDigits[roman[i]] >= romanDigits[roman[i+1]] ) {
-						digital += romanDigits[roman[i]];
+						decimal += romanDigits[roman[i]];
 					} else {
-						digital += romanDigits[roman[i+1]] - romanDigits[roman[i]] 
+						decimal += romanDigits[roman[i+1]] - romanDigits[roman[i]] 
 						i++;
 					}
 				} else {
-					digital += romanDigits[roman[i]];
+					decimal += romanDigits[roman[i]];
 				}
 			}
-			return digital;
+			return decimal;
 		}
 	}
 })();
 
-// console.log(RomaonNumerals.keys());
-// console.log(RomanNumerals.entries());
-console.log(RomanNumerals.fromRoman('VIII'));
 
+console.log(RomanNumerals.toRoman(1000), 'M')
+console.log(RomanNumerals.toRoman(999), "CMXCIX")
+console.log(RomanNumerals.toRoman(4), 'IV')
+console.log(RomanNumerals.toRoman(1), 'I')
+console.log(RomanNumerals.toRoman(1991), 'MCMXCI')
+console.log(RomanNumerals.toRoman(2006), 'MMVI')
+console.log(RomanNumerals.toRoman(2020), 'MMXX')
+
+console.log(RomanNumerals.fromRoman('XXI'), 21)
+console.log(RomanNumerals.fromRoman('I'), 1)
+console.log(RomanNumerals.fromRoman('III'), 3)
+console.log(RomanNumerals.fromRoman('IV'), 4)
+console.log(RomanNumerals.fromRoman('MMVII'), 2007)
+console.log(RomanNumerals.fromRoman('MDCLXIX'), 1669)
 /* Output
 
+M M
+CMXCIX CMXCIX
+IV IV
+I I
+MCMXCI MCMXCI
+MMVI MMVI
+MMXX MMXX
+21 21
+1 1
+3 3
+4 4
+2007 2007
+1669 1669
 */
