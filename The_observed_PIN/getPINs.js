e88dfd38c427a36a3dde124f03dfe2bd67fe67f8
Francis Maile <francismaile@gmail.com>
2019-01-20 15:17:14 -0800

    working but going to make it better

--- The_observed_PIN/getPINs.js
+++ The_observed_PIN/getPINs.js
@@ -1,40 +1,42 @@
 function getPINs(observed) {
   // TODO: This is your job, detective!
-	let keys = [];
-	let variations = [];
+	let keys = Array(observed.length);
 
 	// get all possible key presses
-	[...observed].forEach( cur => {
+	[...observed].forEach( (cur, idx) => {
+		keys[idx] = [];
 		const x = parseInt(cur);
-		if( !keys.includes(x) ) keys.push(x);
+		if( !keys[idx].includes(x) ) keys[idx].push(x);
 		if( x === 0 && !keys.includes(8)  ) {
-			keys.push(8);
+			keys[idx].push(8);
 		} else {
-			if( x === 8 && !keys.includes(0) ) keys.push(0);
-			if( x % 3 > 0 && !keys.includes(x+1) ) keys.push( x + 1 );
-			if( x % 3 === 0 || x % 3 === 2 && !keys.includes(x-1) ) keys.push(x - 1);
-			if( x <= 6 && !keys.includes(x+3) ) keys.push(x + 3);
-			if( x > 3 && !keys.includes(x-3) ) keys.push(x - 3);
+			if( x === 8 && !keys[idx].includes(0) ) keys[idx].push(0);
+			if( x % 3 > 0 && !keys[idx].includes(x+1) ) keys[idx].push( x + 1 );
+			if( x % 3 === 0 || x % 3 === 2 && !keys[idx].includes(x-1) ) keys[idx].push(x - 1);
+			if( x <= 6 && !keys[idx].includes(x+3) ) keys[idx].push(x + 3);
+			if( x > 3 && !keys[idx].includes(x-3) ) keys[idx].push(x - 3);
 		}
 	});
 	// convert to strings
-	keys = keys.map(String);
-	return keys;
+	keys = keys.map( keyChoices => keyChoices.map(String) );
+	// return keys;
 
-	// produce all possible permutations
-	for( let i = 0; i < keys.length; i++ ) {
-		for( let n = 0; n < keys.length; n++ ) {
-			variations.push( keys[i] + keys[n] );
+	let variations = [[]];
+	for( i = 0; i < keys.length; i++ ) {
+		let currentSubArray = keys[i];
+		let temp = [];
+		for( j = 0; j < variations.length; j++ ) {
+			for( k = 0; k < currentSubArray.length; k++ ) {
+				temp.push( variations[j] + currentSubArray[k] );
+			}
 		}
+		variations = temp;
 	}
 	return variations;
 }
 
-// console.log('8', getPINs('8') );
-// console.log('11', getPINs('11') );
+console.log('8', getPINs('8') );
+console.log('11', getPINs('11') );
 console.log('369', getPINs('369') );
-// console.log('1057', getPINs('1057') );
-// console.log('1357', getPINs('1357') );
 
-/* Output
-*/
+
