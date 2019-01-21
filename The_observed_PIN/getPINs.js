eefdf303fd2713c5f86a9694db0e6438185ea83e
Francis Maile <francismaile@gmail.com>
2019-01-20 16:06:09 -0800

    final submitted version

--- The_observed_PIN/getPINs.js
+++ The_observed_PIN/getPINs.js
@@ -1,25 +1,10 @@
 function getPINs(observed) {
   // TODO: This is your job, detective!
-	let keys = Array(observed.length);
+	let keys = [];
+	const alternatives = [ ['8'], ['2', '4'], ['1', '3', '5'], ['2', '6'], ['1', '5', '7'], ['2', '4', '6', '8'], ['3', '5', '9'], ['4', '8'], ['5', '7', '9',  '0'], ['6', '8'] ];
 
 	// get all possible key presses
-	[...observed].forEach( (cur, idx) => {
-		keys[idx] = [];
-		const x = parseInt(cur);
-		if( !keys[idx].includes(x) ) keys[idx].push(x);
-		if( x === 0 && !keys.includes(8)  ) {
-			keys[idx].push(8);
-		} else {
-			if( x === 8 && !keys[idx].includes(0) ) keys[idx].push(0);
-			if( x % 3 > 0 && !keys[idx].includes(x+1) ) keys[idx].push( x + 1 );
-			if( x % 3 === 0 || x % 3 === 2 && !keys[idx].includes(x-1) ) keys[idx].push(x - 1);
-			if( x <= 6 && !keys[idx].includes(x+3) ) keys[idx].push(x + 3);
-			if( x > 3 && !keys[idx].includes(x-3) ) keys[idx].push(x - 3);
-		}
-	});
-	// convert to strings
-	keys = keys.map( keyChoices => keyChoices.map(String) );
-	// return keys;
+	[...observed].forEach( cur => keys.push([cur,...alternatives[parseInt(cur)]]) );
 
 	let variations = [[]];
 	for( i = 0; i < keys.length; i++ ) {
@@ -35,8 +20,7 @@ function getPINs(observed) {
 	return variations;
 }
 
+
 console.log('8', getPINs('8') );
 console.log('11', getPINs('11') );
 console.log('369', getPINs('369') );
-
-
